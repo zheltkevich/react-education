68a6f2cc668785774b1663aedea12954d12f2080
Aleksandr <zheltkevich42@gmail.com>
2023-02-07 20:51:22 +0300

    Add custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useMemo} from "react"
+import {usePosts} from './hooks/usePosts'
 import './styles/App.css'
 // import Counter from "./components/11111/Counter"
 // import ClassCounter from "./components/11111/ClassCounter"
@@ -32,15 +33,7 @@ function App() {
     const [posts, setPosts] = useState(POSTS)
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-
-    const sortedPosts = useMemo(()=> {
-        if (filter.sort) return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        return posts
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPosts])
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
