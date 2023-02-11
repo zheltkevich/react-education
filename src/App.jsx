a88690e9ccf3449c5b073756e41fa2c24e7ec300
Aleksandr <zheltkevich42@gmail.com>
2023-02-11 16:08:09 +0300

    Update routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './styles/main.css'
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import About from "./pages/About"
 import Posts from "./pages/Posts"
-// import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound"
 import AppNavbar from "./components/ui/navbar/AppNavbar"
 
 function App() {
@@ -14,11 +14,11 @@ function App() {
                 <Routes>
                     <Route path="/about" element={<About />} />
                     <Route path="/posts" element={<Posts />} />
-                    {/* <Route path="*" element={<NotFound />} /> */}
-                    <Route
+                    <Route path="*" element={<NotFound />} />
+                    {/* <Route
                         path="*"
                         element={<Navigate to="/posts" replace />}
-                    />
+                    /> */}
                 </Routes>
             </div>
         </BrowserRouter>
