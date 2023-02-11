7dcf6b0472bef0c94d8936619c6689c24736f34d
Aleksandr <zheltkevich42@gmail.com>
2023-02-11 16:12:12 +0300

    Replace routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import React from "react"
 import './styles/main.css'
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
-import About from "./pages/About"
-import Posts from "./pages/Posts"
-import NotFound from "./pages/NotFound"
 import AppNavbar from "./components/ui/navbar/AppNavbar"
+import AppRouter from "./components/AppRouter"
 
 function App() {
     return (
         <BrowserRouter>
             <AppNavbar />
-            <div className="container">
-                <Routes>
-                    <Route path="/about" element={<About />} />
-                    <Route path="/posts" element={<Posts />} />
-                    <Route path="*" element={<NotFound />} />
-                    {/* <Route
-                        path="*"
-                        element={<Navigate to="/posts" replace />}
-                    /> */}
-                </Routes>
-            </div>
+            <AppRouter />
         </BrowserRouter>
     )
 }
