fadf918ced2a931b976500b408b7ef9d68c7a388
Aleksandr <zheltkevich42@gmail.com>
2023-02-11 18:53:06 +0300

    Add login page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import About from "../pages/About"
 import Posts from "../pages/Posts"
-import NotFound from "../pages/NotFound"
+// import NotFound from "../pages/NotFound"
 import PostIdPage from '../pages/PostIdPage'
+import Login from '../pages/Login'
 
 const AppRouter = () => {
-  return (
-    <div className="container">
-        <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/posts/:id" element={<PostIdPage />} />
-            <Route path="*" element={<NotFound />} />
-            {/* <Route
-                path="*"
-                element={<Navigate to="/posts" replace />}
-            /> */}
-        </Routes>
-    </div>
-  )
+    const authenticated = false;
+
+    return (
+        <div className="container">
+            {
+                authenticated
+                    ?
+                        <Routes>
+                            <Route path="/about" element={<About />} />
+                            <Route path="/posts" element={<Posts />} />
+                            <Route path="/posts/:id" element={<PostIdPage />} />
+                            <Route
+                                path="*"
+                                element={<Navigate to="/posts" replace />}
+                            />
+                        </Routes>
+                    :
+                        <Routes>
+                            <Route path="/login" element={<Login />} />
+                            <Route
+                                path="*"
+                                element={<Navigate to="/login" replace />}
+                            />
+                        </Routes>
+            }
+        </div>
+    )
 }
 
 export default AppRouter
